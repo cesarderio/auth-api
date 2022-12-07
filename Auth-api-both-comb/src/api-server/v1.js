5f96a9d68777c2c51992cf30fe1a3a86e4dd4e99
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-12-07 15:46:42 -0800

    working on combining api-server with auth-serverinside of auth-api

--- Auth-api-both-comb/src/api-server/v1.js
+++ Auth-api-both-comb/src/api-server/v1.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const dataModules = require('../models');
+const dataModules = require('../../models');
 
 const router = express.Router();
 
---
+++ Auth-api-both-comb/src/server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// api-server
+
+const express = require('express');
+
+// const notFoundHandler = require('./error-handlers/404.js');
+const errorHandler = require('./error-handlers/500.js');
+// const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const logger = require('./middleware/logger.js');
+
+
+const v1Routes = require('./routes/v1.js');
+// Esoteric Resources-server
+const authRoutes = require('./auth/routes.js');
+///--------------------------------------
+
+const app = express();
+
+// 3rd Party Resources
+// const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// App Level MW- auth-server
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+//------------------------
+
+//---API-server-------------
+app.use(express.json());
+app.use(logger);
+
+//------------auth-server------
+// Routes
+app.use(authRoutes);
+//--------------
+
+app.use('/api/v1', v1Routes); // http://localhost:3000/api/v1/clothes
+
+app.use('*', notFound);
+app.use(errorHandler);
+//---------------------------
+//----Auth-Server----------
+// Catchalls
+// app.use(notFound);
+// app.use(errorHandler);
+
+//-----------------
+
+module.exports = {
+  server: app,
+  start: () => app.listen(PORT, console.log('server running on port', PORT)),
+};
